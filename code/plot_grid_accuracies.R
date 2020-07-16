3c20049501d0f2ae5bae2adfc743e43bc8176b17
RoccoA97 <roccoardino@gmail.com>
2020-07-16 15:42:40 +0200

    Presentation updated and new code added

--- code/plot_grid_accuracies.R
+++ code/plot_grid_accuracies.R
@@ -1,3 +1,5 @@
+################################################################################
+################################################################################
 plot_batch_epochs_grid <- function(file.txt, repr_plot_width=4, repr_plot_height=4, repr_plot_res=250) {
 
 	options(repr.plot.width=repr_plot_width, repr.plot.height=repr_plot_height, repr.plot.res=repr_plot_res)
@@ -26,10 +28,14 @@ plot_batch_epochs_grid <- function(file.txt, repr_plot_width=4, repr_plot_height
 	coord_cartesian(expand=FALSE)
 
 }
+################################################################################
+################################################################################
 
 
 
-plot_unit1_unit2_grid <- function(file.txt, repr_plot_width=12, repr_plot_height=4, repr_plot_res=250) {
+################################################################################
+################################################################################
+plot_unit_grid <- function(file.txt, repr_plot_width=12, repr_plot_height=4, repr_plot_res=250) {
 
 	par(mfrow=c(1,3))
 	options(repr.plot.width=repr_plot_width, repr.plot.height=repr_plot_height, repr.plot.res=repr_plot_res)
@@ -106,12 +112,103 @@ plot_unit1_unit2_grid <- function(file.txt, repr_plot_width=12, repr_plot_height
 	theme(plot.title=element_text(hjust=0.5))               +
 	coord_cartesian(expand=FALSE)
 
+	grid.arrange(g1, g2, g3, nrow=1)
+
+}
+################################################################################
+################################################################################
+
+
+
+################################################################################
+################################################################################
+plot_dropout_grid <- function(file.txt, repr_plot_width=12, repr_plot_height=4, repr_plot_res=250) {
+
+	par(mfrow=c(1,3))
+	options(repr.plot.width=repr_plot_width, repr.plot.height=repr_plot_height, repr.plot.res=repr_plot_res)
+
+
+	xlab  <- "Dropout Layer 1"
+	ylab  <- "Dropout Layer 2"
+	title <- "(Dropout 1, Dropout 2, Dropout 3 = 0.1) tuning"
+
+	grid    <- read.table(file.txt[1])
+	xticks  <- factor(grid[,1])
+	yticks  <- factor(grid[,2])
+	accs    <- grid[,"Mean_acc"]
+	grid.df <- data.frame(xticks, yticks, accs)
+
+	g1 <-
+	ggplot(data=grid.df, mapping=aes(x=xticks, y=yticks))   +
+	geom_tile(aes(fill=accs), colour="white")               +
+	geom_text(aes(label = sprintf("%1.4f", accs)), vjust=1) +
+	scale_fill_continuous(low="yellow",high="red")          +
+	theme_bw()                                              +
+	theme(legend.position="none")                           +
+	xlab(xlab)                                              +
+	ylab(ylab)                                              +
+	ggtitle(title)                                          +
+	theme(plot.title=element_text(hjust=0.5))               +
+	coord_cartesian(expand=FALSE)
+
+
+	xlab  <- "Dropout Layer 1"
+	ylab  <- "Dropout Layer 2"
+	title <- "(Dropout 1, Dropout 2, Dropout 3 = 0.2) tuning"
+
+	grid    <- read.table(file.txt[2])
+	xticks  <- factor(grid[,1])
+	yticks  <- factor(grid[,2])
+	accs    <- grid[,"Mean_acc"]
+	grid.df <- data.frame(xticks, yticks, accs)
+
+	g2 <-
+	ggplot(data=grid.df, mapping=aes(x=xticks, y=yticks))   +
+	geom_tile(aes(fill=accs), colour="white")               +
+	geom_text(aes(label = sprintf("%1.4f", accs)), vjust=1) +
+	scale_fill_continuous(low="yellow",high="red")          +
+	theme_bw()                                              +
+	theme(legend.position="none")                           +
+	xlab(xlab)                                              +
+	ylab(ylab)                                              +
+	ggtitle(title)                                          +
+	theme(plot.title=element_text(hjust=0.5))               +
+	coord_cartesian(expand=FALSE)
+
+
+	xlab  <- "Dropout Layer 1"
+	ylab  <- "Dropout Layer 2"
+	title <- "(Dropout 1, Dropout 2, Dropout 3 = 0.3) tuning"
+
+	grid    <- read.table(file.txt[3])
+	xticks  <- factor(grid[,1])
+	yticks  <- factor(grid[,2])
+	accs    <- grid[,"Mean_acc"]
+	grid.df <- data.frame(xticks, yticks, accs)
+
+	g3 <-
+	ggplot(data=grid.df, mapping=aes(x=xticks, y=yticks))   +
+	geom_tile(aes(fill=accs), colour="white")               +
+	geom_text(aes(label = sprintf("%1.4f", accs)), vjust=1) +
+	scale_fill_continuous(low="yellow",high="red")          +
+	theme_bw()                                              +
+	theme(legend.position="none")                           +
+	xlab(xlab)                                              +
+	ylab(ylab)                                              +
+	ggtitle(title)                                          +
+	theme(plot.title=element_text(hjust=0.5))               +
+	coord_cartesian(expand=FALSE)
+
 	grid.arrange(g1, g2, g3, nrow = 1)
 
 }
+################################################################################
+################################################################################
 
 
 
+################################################################################
+################################################################################
 plot_actI_actF_grid <- function(file.txt, repr_plot_width=12, repr_plot_height=4, repr_plot_res=250) {
 
 	options(repr.plot.width=repr_plot_width, repr.plot.height=repr_plot_height, repr.plot.res=repr_plot_res)
@@ -140,14 +237,21 @@ plot_actI_actF_grid <- function(file.txt, repr_plot_width=12, repr_plot_height=4
 	coord_cartesian(expand=FALSE)
 
 }
+################################################################################
+################################################################################
 
 
 
-plot_optimizer_grid <- function(file.txt, repr_plot_width=16, repr_plot_height=4, repr_plot_res=250) {
+################################################################################
+################################################################################
+plot_optimizer_rms_grid <- function(file.txt, repr_plot_width=16, repr_plot_height=4, repr_plot_res=250) {
 
 	par(mfrow=c(1,4))
 	options(repr.plot.width=repr_plot_width, repr.plot.height=repr_plot_height, repr.plot.res=repr_plot_res)
 
+	xlab  <- "learning rate"
+	ylab  <- "rho"
+	title <- "RMSProp(lr, rho) tuning"
 
 	grid    <- read.table(file.txt[1])
 	xticks  <- factor(grid[,1])
@@ -167,68 +271,101 @@ plot_optimizer_grid <- function(file.txt, repr_plot_width=16, repr_plot_height=4
 	theme(plot.title=element_text(hjust=0.5))                 +
 	coord_cartesian(expand=FALSE)
 
+}
+################################################################################
+################################################################################
+
+
+
+################################################################################
+################################################################################
+plot_optimizer_adam_grid <- function(file.txt, repr_plot_width=16, repr_plot_height=4, repr_plot_res=250) {
+
+	par(mfrow=c(1,3))
+	options(repr.plot.width=repr_plot_width, repr.plot.height=repr_plot_height, repr.plot.res=repr_plot_res)
+
+
+	xlab  <- "learning rate"
+	ylab  <- "beta 1"
+	title <- "ADAM(lr, beta1, beta2 = 0.990) tuning"
+
+	grid    <- read.table(file.txt[1])
+	xticks  <- factor(grid[,2])
+	yticks  <- factor(grid[,3])
+	accs    <- grid[,"Mean_acc"]
+	grid.df <- data.frame(xticks, yticks, accs)
+
+	g1 <-
+	ggplot(data=grid.df, mapping=aes(x=xticks, y=yticks))   +
+	geom_tile(aes(fill=accs), colour="white")               +
+	geom_text(aes(label = sprintf("%1.4f", accs)), vjust=1) +
+	scale_fill_continuous(low="yellow",high="red")          +
+	theme_bw()                                              +
+	theme(legend.position="none")                           +
+	xlab(xlab)                                              +
+	ylab(ylab)                                              +
+	ggtitle(title)                                          +
+	theme(plot.title=element_text(hjust=0.5))               +
+	coord_cartesian(expand=FALSE)
+
+
+	xlab  <- "learning rate"
+	ylab  <- "beta 1"
+	title <- "ADAM(lr, beta1, beta2 = 0.995) tuning"
 
 	grid    <- read.table(file.txt[2])
-	xticks  <- factor(grid[,1])
-	yticks  <- factor(grid[,2])
+	xticks  <- factor(grid[,2])
+	yticks  <- factor(grid[,3])
 	accs    <- grid[,"Mean_acc"]
 	grid.df <- data.frame(xticks, yticks, accs)
 
-	ggplot(data=grid.df, mapping=aes(x=xticks, y=yticks))     +
-	geom_tile(aes(fill=accs), colour="white")                 +
-	geom_text(aes(label = sprintf("%1.4f", accs)), vjust=1)   +
-	scale_fill_continuous(low="yellow",high="red")            +
-	theme_bw()                                                +
-	theme(legend.position="none")                             +
-	xlab(xlab)                                                +
-	ylab(ylab)                                                +
-	ggtitle(title)                                            +
-	theme(plot.title=element_text(hjust=0.5))                 +
+	g2 <-
+	ggplot(data=grid.df, mapping=aes(x=xticks, y=yticks))   +
+	geom_tile(aes(fill=accs), colour="white")               +
+	geom_text(aes(label = sprintf("%1.4f", accs)), vjust=1) +
+	scale_fill_continuous(low="yellow",high="red")          +
+	theme_bw()                                              +
+	theme(legend.position="none")                           +
+	xlab(xlab)                                              +
+	ylab(ylab)                                              +
+	ggtitle(title)                                          +
+	theme(plot.title=element_text(hjust=0.5))               +
 	coord_cartesian(expand=FALSE)
 
+
+	xlab  <- "learning rate"
+	ylab  <- "beta 1"
+	title <- "ADAM(lr, beta1, beta2 = 0.999) tuning"
 
 	grid    <- read.table(file.txt[3])
-	xticks  <- factor(grid[,1])
-	yticks  <- factor(grid[,2])
+	xticks  <- factor(grid[,2])
+	yticks  <- factor(grid[,3])
 	accs    <- grid[,"Mean_acc"]
 	grid.df <- data.frame(xticks, yticks, accs)
 
-	ggplot(data=grid.df, mapping=aes(x=xticks, y=yticks))     +
-	geom_tile(aes(fill=accs), colour="white")                 +
-	geom_text(aes(label = sprintf("%1.4f", accs)), vjust=1)   +
-	scale_fill_continuous(low="yellow",high="red")            +
-	theme_bw()                                                +
-	theme(legend.position="none")                             +
-	xlab(xlab)                                                +
-	ylab(ylab)                                                +
-	ggtitle(title)                                            +
-	theme(plot.title=element_text(hjust=0.5))                 +
+	g3 <-
+	ggplot(data=grid.df, mapping=aes(x=xticks, y=yticks))   +
+	geom_tile(aes(fill=accs), colour="white")               +
+	geom_text(aes(label = sprintf("%1.4f", accs)), vjust=1) +
+	scale_fill_continuous(low="yellow",high="red")          +
+	theme_bw()                                              +
+	theme(legend.position="none")                           +
+	xlab(xlab)                                              +
+	ylab(ylab)                                              +
+	ggtitle(title)                                          +
+	theme(plot.title=element_text(hjust=0.5))               +
 	coord_cartesian(expand=FALSE)
 
-
-	grid    <- read.table(file.txt[4])
-	xticks  <- factor(grid[,1])
-	yticks  <- factor(grid[,2])
-	accs    <- grid[,"Mean_acc"]
-	grid.df <- data.frame(xticks, yticks, accs)
-
-	ggplot(data=grid.df, mapping=aes(x=xticks, y=yticks))     +
-	geom_tile(aes(fill=accs), colour="white")                 +
-	geom_text(aes(label = sprintf("%1.4f", accs)), vjust=1)   +
-	scale_fill_continuous(low="yellow",high="red")            +
-	theme_bw()                                                +
-	theme(legend.position="none")                             +
-	xlab(xlab)                                                +
-	ylab(ylab)                                                +
-	ggtitle(title)                                            +
-	theme(plot.title=element_text(hjust=0.5))                 +
-	coord_cartesian(expand=FALSE)
+	grid.arrange(g1, g2, g3, nrow=1)
 
 }
+################################################################################
+################################################################################
 
 
 
-
+################################################################################
+################################################################################
 plot_l1_l2_grid <- function(file.txt, repr_plot_width=16, repr_plot_height=4, repr_plot_res=250) {
 
 	par(mfrow=c(1,4))
@@ -311,3 +448,5 @@ plot_l1_l2_grid <- function(file.txt, repr_plot_width=16, repr_plot_height=4, re
 	coord_cartesian(expand=FALSE)
 
 }
+################################################################################
+################################################################################
